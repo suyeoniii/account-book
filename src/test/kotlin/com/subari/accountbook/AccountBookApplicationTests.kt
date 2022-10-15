8fd2292ee4987642b74eb3b6158db58ba45206a1
suyeoniii <ssy4230@naver.com>
2022-10-15 23:32:06 +0900

    Initial commit

---
+++ src/test/kotlin/com/subari/accountbook/AccountBookApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.subari.accountbook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccountBookApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
